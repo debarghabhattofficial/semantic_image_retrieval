c203a70082a7e0d8773e181b973f3d6248ac9fff
debarghabhattofficial <dbhattacharjee@monarchtractor.com>
2023-11-21 09:24:04 +0530

    Added a post processing method.
    Added a method to add the generated caption at the bottom of the image.

--- src/image_captioning.py
+++ src/image_captioning.py
@@ -4,7 +4,8 @@ import argparse
 import torch
 import torchvision.transforms as transforms
 from lavis.models import load_model_and_preprocess
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -13,6 +14,11 @@ def parse_args():
         "--img_path",
         type=str,
         help="Path to input image."
+    )
+    parser.add_argument(
+        "--out_dir",
+        type=str,
+        help="Path to output directory to store results."
     )
     parser.add_argument(
         "--verbose", 
@@ -43,38 +49,101 @@ def read_image(img_path):
     return img_tensor
 
 
+def add_caption(img_path, 
+                caption_text, 
+                out_dir):
+    """
+    This method adds a caption at the bottom of
+    an image.
+    """
+    # # Step 1: Read input image.
+    img_pil = Image.open(img_path)
+
+    # Create a rectangular canvas with off-white 
+    # background color.
+    canvas = Image.new(
+        "RGB", 
+        (img_pil.width, img_pil.height + 100), 
+        (245, 245, 245)
+    )
+
+    # Copy the input image on the top portion of 
+    # the canvas.
+    canvas.paste(img_pil, (0, 0))
+
+    # Add a caption at the bottom of the canvas.
+    draw = ImageDraw.Draw(canvas)
+    try:
+        font = ImageFont.truetype(
+            "/usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf", 
+            size=20
+        )
+    except IOError:
+        font = ImageFont.load_default()
+    caption_position = (10, img_pil.height + 10)
+    caption_font_color = (0, 0, 0)  # Black
+    draw.text(
+        caption_position, 
+        caption_text, 
+        font=font, 
+        fill=caption_font_color
+    )
+
+    # Save the modified image having caption.
+    if not out_dir is None:
+        # Create directory is it does not exist.
+        if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+        # Set the output path.
+        out_path = os.path.join(
+            out_dir, os.path.basename(img_path)
+        )
+        # Save the modified image having caption.
+        canvas.save(out_path)
+
+    return
+
+
 def main():
     args = parse_args()
-    img_path = args.img_path
-    verbose = args.verbose
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Loads BLIP caption base model, with finetuned checkpoints on MSCOCO captioning dataset.
+    # Loads BLIP/BLIP2 caption base model, with finetuned checkpoints on MSCOCO captioning dataset.
     # this also loads the associated image processors
     model, vis_processors, _ = load_model_and_preprocess(
-        name="blip2_t5", 
-        model_type="caption_coco_flant5xl", 
+        name="blip_caption",  # "blip2_t5", 
+        model_type="base_coco",  # "caption_coco_flant5xl", 
         is_eval=True, 
         device=device
     )
 
-    # Preprocess the image and return tensor.
     image = None
-    if os.path.exists(img_path):
+    if os.path.exists(args.img_path):
         print("Image found.")
-        image = read_image(img_path).to(device)
-        if verbose:
-            print(f"image_shape: {image.shape}")
-            print(f"image: \n{image}")
+        # Preprocess the image and return tensor.
+        img_tensor = read_image(args.img_path).to(device)
+        if args.verbose:
+            print(f"img_tensor.shape: {img_tensor.shape}")
+            print(f"img_tensor: \n{img_tensor}")
             print("-" * 75)
+
+        # Generate caption for the image.
+        output = model.generate({"image": img_tensor})
+        print(f"output: {output}")
+        print("-" * 75)
+
+        # Extract the caption from the output and
+        # add to the bottom of the image.
+        add_caption(
+            img_path=args.img_path, 
+            caption_text=output[0],
+            out_dir=args.out_dir
+        )
     else:
         print("Image not found.")
 
-    # Generate caption for the image.
-    output = model.generate({"image": image})
-    print(f"output: {output}")
-    print("-" * 75)
+    return
 
 
 if __name__ == "__main__":
