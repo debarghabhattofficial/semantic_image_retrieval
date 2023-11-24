348377dfe701a361ccf1e820d055584011d773d2
debarghabhattofficial <dbhattacharjee@monarchtractor.com>
2023-11-24 21:08:01 +0530

    Updated test.sh bash script for vqa task.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,8 @@
 cd "../src"
 
 python test.py \
-    --cfg_path "../cfg/image_captioning.yaml" \
+    --cfg_path "../cfg/vqa-blip.yaml" \
     --dataset_path "../input_images/test_dataset" \
-    --img_path "../input_images/test_sample_1.jpg" \
+    --img_path "../input_images/flare_200.png" \
     --out_dir "../results/image_captioning/blip/test_dataset" \
-    --infer_batch \
     --verbose
