43cf045c0a973849ef9e7d43ac2c806c5643c81b
debarghabhattofficial <dbhattacharjee@monarchtractor.com>
2023-11-30 09:37:59 +0530

    Tested new input by changing path in 'test.sh' script.

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,6 @@ cd "../src"
 python test.py \
     --cfg_path "../cfg/vqa-blip.yaml" \
     --dataset_path "../input_images/test_dataset" \
-    --img_path "../input_images/test_sample_1.jpg" \
+    --img_path "../input_images/row_runner_2_vines_april_block1_0102.jpg" \
     --out_dir "../results/vqa/blip/" \
     --verbose
