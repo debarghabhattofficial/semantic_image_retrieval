e3dde465f7f02b0857d67456a66384cfee8f134c
debarghabhattofficial <dbhattacharjee@monarchtractor.com>
2023-12-20 10:24:47 +0530

    Added new flags.
    Added flags to computing and saving cross-modality scores.

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,7 @@ python test.py \
     --cfg_path "../cfg/feat_extract-blip.yaml" \
     --dataset_path "../input_images/auto_image_labelling/bushy-patchy" \
     --img_path "../input_images/test_sample_1.jpg" \
-    --text_input "a woman in a bathing suit holding a pink umbrella" \
+    --text_input "bushy" "patchy" \
     --out_dir "../results/auto_image_labelling/blip/bushy-patchy" \
     --plot_dir "../plots/auto_image_labelling/blip/bushy-patchy" \
     --infer_batch \
@@ -15,4 +15,6 @@ python test.py \
     --vis_pca \
     --save_plots \
     --save_embeds \
+    --compute_similarity \
+    --save_sim_scores \
     --verbose
